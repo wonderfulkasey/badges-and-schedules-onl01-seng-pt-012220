6b621723b41d2e621a0985582f82ff51d24b868d
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-29 16:51:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ attendees = ["Edsger","Ada","Charles","Alan", "Grace","Linus","Matz"]
 
 
 def batch_badge_creator(attendees)
-  attendees.each do |attendee|
+  attendees.map do |attendee|
   "Hello, my name is #{attendee}."
   end
 end
