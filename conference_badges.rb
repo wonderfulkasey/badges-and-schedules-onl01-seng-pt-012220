3c3d2459d63b6d05a2edb7ffaef5a854cb659cea
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-29 17:02:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,8 @@ end
 
 def assign_rooms(attendees)
   attendees.each_with_index.map do |attendee, index|
-  "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  "Hello, #{attendee}! You'll be assigned to room #{index+1}!"\
+end
 end
 
 
