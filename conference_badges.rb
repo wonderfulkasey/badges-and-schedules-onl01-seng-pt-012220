d42f1ef630de895984a3a055ac18423e67697d52
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-29 16:27:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,14 @@
 # Write your code here.
 
-badge_maker("Kasey")
-=> "Hello, my name is Kasey."
+#badge_maker("Kasey")
+#=> "Hello, my name is Kasey."
+
+def batch_badge_creator
+  
+  badge_maker
+  
+  assign_rooms
+  
+  => "Hello, ! You'll be assigned to room !"
+  
+end
