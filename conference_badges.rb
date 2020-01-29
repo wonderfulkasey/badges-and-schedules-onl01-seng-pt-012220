721a76db26a7d2a429886aed1a2751ae7c65326f
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-29 17:07:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,7 +24,9 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
 end
-  assign_rooms(attendees).each do 
+  assign_rooms(attendees).each do |assignment|
+    
+    
 
 
 
