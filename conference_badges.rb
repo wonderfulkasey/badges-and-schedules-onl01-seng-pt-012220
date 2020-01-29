34934fbfa71ff491f10dd82fd58c44306eff7262
Kasey Arnold <kaseyarnold15@gmail.com>
2020-01-29 16:39:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
-attendees = []
+attendees = ["Edsger","Ada","Charles","Alan", "Grace","Linus","Matz"]
 
 def batch_badge_creator(attendees)
   
